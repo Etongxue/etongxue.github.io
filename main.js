194fcbf2d2a95b94db46784fa2dded02a098624b
Etongxue <1341361765@qq.com>
2022-04-12 21:55:05 +0800

    f

---
+++ main.js
@@ -0,0 +1,3 @@
+var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
+console.log('\n' + ' %c PersonalWebSite by 聆听远方 ' + ' %c https://ltyuanfang.cn ' + '\n' + '\n',
+	'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
